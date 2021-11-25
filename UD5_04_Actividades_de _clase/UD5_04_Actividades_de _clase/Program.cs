179a6fea70a988bfb16ff1f8e7c0d4a85b8621a1
ValdesRiveraDaniel <73697457+ValdesRiveraDaniel@users.noreply.github.com>
2021-11-25 21:34:24 +0100

    commit

---
+++ UD5_04_Actividades_de _clase/UD5_04_Actividades_de _clase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD5_04_Actividades_de__clase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("hola, escribe algo: ");
+			String texto = Console.ReadLine();
+			Console.WriteLine(texto);
+		}
+	}
+}
